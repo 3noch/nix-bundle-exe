f19bda5a4b57c86252b50919fac9bb3579faa91b
HuangYi <huang@crypto.com>
2023-02-23 01:08:35 +0800

    callPackage style

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
-{ pkgs
+{ lib
+, writeScriptBin
+, buildPackages
+, stdenv
+, glibc
+, runCommand
+, nukeReferences
 , bin_dir ? "bin"
 , exe_dir ? "exe"
-, lib_dir ? if pkgs.stdenv.isDarwin then "Frameworks/Library.dylib" else "lib"
+, lib_dir ? if stdenv.isDarwin then "Frameworks/Library.dylib" else "lib"
 }:
 path:
 # May be:
@@ -9,41 +15,41 @@ path:
 #  2) a path to a directory containing bin/, or
 #  3) a path to an executable.
 let
-  print-needed-elf = pkgs.writeScriptBin
+  print-needed-elf = writeScriptBin
     "print-needed-elf"
-    '''${pkgs.python3}'/bin/python ${./print_needed_elf.py} "$@"'';
+    '''${buildPackages.python3}'/bin/python ${./print_needed_elf.py} "$@"'';
 
   cfg =
-    if pkgs.stdenv.isDarwin then
+    if stdenv.isDarwin then
       {
-        deps = with pkgs; [ darwin.binutils darwin.sigtool ];
+        deps = with buildPackages; [ darwin.binutils darwin.sigtool ];
         script = "bash ${./bundle-macos.sh}";
       }
-    else if pkgs.stdenv.isLinux then
+    else if stdenv.isLinux then
       {
-        deps = [ pkgs.glibc print-needed-elf ];
+        deps = [ glibc print-needed-elf ];
         script = "bash ${./bundle-linux.sh}";
       }
     else
       throw "Unsupported platform: only darwin and linux are supported";
 
-  name = if pkgs.lib.isDerivation path then path.name else builtins.baseNameOf path;
+  name = if lib.isDerivation path then path.name else builtins.baseNameOf path;
   overrideEnv = name: value: if value == null then "" else "export ${name}='${value}'";
 in
-pkgs.runCommand "bundle-${name}"
+runCommand "bundle-${name}"
 {
-  nativeBuildInputs = cfg.deps ++ [ pkgs.nukeReferences ];
+  nativeBuildInputs = cfg.deps ++ [ buildPackages.nukeReferences ];
 }
   ''
     set -euo pipefail
     export bin_dir='${bin_dir}'
     export exe_dir='${exe_dir}'
     export lib_dir='${lib_dir}'
-    export target_prefix='${pkgs.stdenv.cc.targetPrefix}'
+    export target_prefix='${stdenv.cc.targetPrefix}'
     ${if builtins.pathExists "${path}/bin" then ''
       find '${path}/bin' -type f -executable -print0 | xargs -0 --max-args 1 ${cfg.script} "$out"
     '' else ''
-      ${cfg.script} "$out" ${pkgs.lib.escapeShellArg path}
+      ${cfg.script} "$out" ${lib.escapeShellArg path}
     ''}
     find $out -empty -type d -delete
   ''
